8e36eaa94f547c60e8ff1df2971453d4061c8207
stlynnxx <stevie@eclecticelectronics.com>
2026-01-05 12:06:53 -0600

    Initial Commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template
+import random
+
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+def home():
+
+    return render_template('home.html')
+@app.route('/eclecticAI')
+def eclecticAI():
+    return render_template('eclecticAI.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/quote')
+def quote():
+    return render_template('quote.html')
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+@app.route('/password')
+def password():
+    randoms = []
+    alphabet = ["A","a","B","b","C","c","D","d","E","e"
+                "F","f","G","g","H","h","I","i","J","j"
+                "K","k","L","l","M","m","N","n","O","o"
+                "P","p","Q","q","R","r","S","s"
+                "T","t","U","u","V","v","W","w"
+                "X","x","Y","y","Z","z"]
+    value_one = random.randint(1,9)
+    value_two = random.randint(1,9)
+    value_three = random.randint(1,9)
+    for i in value_three:
+       randoms = [random.choice(alphabet)]
+    
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=6969)
