ff0ce2d415a52990f836a16ecbdc9a7492093f6e
stlynnxx <stevie@eclecticelectronics.com>
2026-09-15 10:07:48 -0500

    SPv2.1.2 fix

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 UPDATE_INFO = {
     "latest": "2.1.2",
     "url": "https://eclecticelectronics.fly.dev/api/download/2.1.2",
-    "sha256": "e6c0292e067e03ed630e765f26ce180004c830fa3241705d1db4872b8fda487a",
+    "sha256": "22814d735170c45f492aaf097f17a23b2258f484ead979517ad90a0084b498b3",
     "Release Date": "9/15/2026",
     "notes": ""
 }
